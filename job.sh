d670e1ebf9e3a1f2fa0028cc0fbb70c1d683f529
csimal <csimal@student.unamur.be>
2018-12-23 12:53:12 +0100

    almost forgot

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=generate_population
 #SBATCH --array=1-200
 #SBATCH --time=01:00:00
-#SBATCH --mem-per-cpu=1024
+#SBATCH --mem-per-cpu=512
 #SBATCH --partition=Student
 echo "Task ID: $SLURM_ARRAY_TASK_ID"
 cp -r tpp/ $LOCALSCRATCH/.
